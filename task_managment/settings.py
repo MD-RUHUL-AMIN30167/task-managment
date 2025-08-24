1223f7d1c5d6bdff7c04cb1a5b74fb354b1bbd88
MD RUHUL AMIN <mdruhulamin@email.com>
2025-08-24 23:52:14 +0700

    ready for deploment

--- task_managment/settings.py
+++ task_managment/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-1ky^_0*r=^w#_*u@6ra!y*trr4c_dw04(eh@5n=4+=3@o_5ndr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com','http://127.0.0.1:8000']
 
 
 # Application definition
--- task_managment/urls.py
+++ task_managment/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("<h1>Welcome to Task Management</h1>")
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path("tasks/",include("tasks.urls")),  
     
 
